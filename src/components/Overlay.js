e218e788dab033662bd4f800e1a48bbb30002134
Jacob Prudent <jake.prudent@gmail.com>
2020-08-11 23:10:39 -0700

    Refactored Cards

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -3,30 +3,16 @@ import closeOverlay from '../utilities/closeOverlay'
 import ButtonGen from './ButtonGen'
 
 const Overlay = props => {
-
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-
     return (
         <div id={props.overlayId} className="h-full transBackground w-0 fixed z-20 left-0 top-0 overflow-x-hidden duration-500 flex justify-center align-center">
-
-            {/* <!-- Button to close the overlay navigation --> */}
             <div className="max-h-screen">
                 <div className="flex flex-col md:flex md:flex-row md:flex-no-wrap my-1 mx-1">
                     <button className="absolute top-0 right-0 p-5 text-6xl text-white outline-none z-30" onClick={() => closeOverlay(props.overlayId)}>&times;</button>
                     <a href={props.liveProjLink} className="w-0 lg:w-1/3 max-screen object-cover object-center bg-black">
-                        <img
-                            src={props.photo}
-                            alt="Tech Sorting Hat Questionnaire Application"
-                            className="max-screen"
-                        />
+                        <img src={props.photo} alt="Tech Sorting Hat Questionnaire Application" className="max-screen" />
                     </a>
-
-
                     <div className="bg-black text-white lg:w-2/3 flex flex-col justify-center py-10 px-20">
-                        <h1 className='text-4xl text-white bold' id="project1" >
-                            {props.title}
-                        </h1>
+                        <h1 className='text-4xl text-white bold' id="project1" >{props.title}</h1>
                         <p className="py-10 text-2xl">{props.description}</p>
                         <div>{props.techs}</div>
                         <div className="flex-row w-full justify-evenly">
@@ -37,7 +23,6 @@ const Overlay = props => {
                 </div>
             </div>
         </div>
-
     )
 }
 export default Overlay
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,67 +1,23 @@
 //Libraries and Components
-import React, { useRef, useEffect, useState } from 'react';
-import openOverlay from '../utilities/openOverlay';
+import React, { useRef, useEffect } from 'react';
 import ContactNav from './ContactNav';
-
+import ProjectCard from './ProjectCard';
 //Images
 import Zebra from './img/zebrastripes.jpg';
 import WhiteCat from './img/whitecat.jpg';
 import Wall from './img/whitewall.jpg';
 
 //Animations
-import { TweenMax, Power1 } from 'gsap';
 import gsaper from '../utilities/gsaper';
-import VisibilitySensor from 'react-visibility-sensor';
-//Visibility Sensor seems to have a conflict with GSAP Where the two fight and neither works. I can place the sensor on something near by though which can help
-
-//Repeated Tailwind
-const CardGen =
-	'bg-black text-white md:w-2/3 flex flex-col relative justify-center py-10 px-10 lg:px-20 ';
-const ButtonGen = 'px-1 py-2 md:py-4 mx-1 border border-white text-white bg-transparent rounded-full hover:bg-primary hover:text-white lg:w-1/2 text-lg outline-none';
-const HeaderGen = 'text-4xl text-white';
-
-//Scroll Function
 
 const Portfolio = () => {
-
-	//State for animation to reveal photos
-	const [iViz2, setIViz2] = useState(false);
-	const [iViz3, setIViz3] = useState(false);
-	const [iViz4, setIViz4] = useState(false);
-
-	//State for rendering subcomponents
-
-
-	//Functions for rendering subcomponents
-
 	let mainHeading = useRef(null);
-	let image2 = useRef(null);
-	let image3 = useRef(null);
-	let image4 = useRef(null);
 	let project1 = useRef(null);
-
-
 	//Functions for animation
 	const executeScroll = () => window.scrollTo(0, project1.current.offsetTop)
-
-	function onChange(isVisible, callback) {
-		callback(isVisible);
-	}
-
-
 	useEffect(() => {
 		gsaper(true, mainHeading);
 	}, []);
-	useEffect(() => {
-		gsaper(iViz2, image2);
-	}, [iViz2]);
-	useEffect(() => {
-		gsaper(iViz3, image3);
-	}, [iViz3]);
-	useEffect(() => {
-		gsaper(iViz4, image4);
-	}, [iViz4]);
-
 	return (
 		<div className="bg-primary ">
 			<section className="sm:flex-col md:flex-row-reverse md:h-screen md:relative">
@@ -69,7 +25,7 @@ const Portfolio = () => {
 					className="md:z-0 max-full relative object-cover object-center md:h-screen md:max-h-screen lg:absolute lg:right-0"
 					src="https://images.unsplash.com/photo-1578836537282-3171d77f8632?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
 					ref={(el) => {
-						image2 = el;
+						mainHeading = el;
 					}}
 					alt="Mountain in the clouds"
 				/>
@@ -90,7 +46,6 @@ const Portfolio = () => {
 					<div className="display flex">
 						<button
 							className="px-1 py-2 md:py-4 mx-1 border border-white text-white bg-transparent rounded-full hover:bg-primary hover:text-white lg:w-1/2 text-lg outline-none"
-							//I need to write a script to scroll to the first project
 							onClick={executeScroll}
 						>
 							View My Projects
@@ -103,74 +58,39 @@ const Portfolio = () => {
 							About Me
 					</button> */}
 					</div>
-
-
 				</div>
 			</section>
 			<section ref={project1}>
-				<div className="flex flex-col md:flex md:flex-row md:flex-no-wrap">
-					<img
-						className="md:w-1/3 opacity-0 max-full relative object-cover object-center"
-						src={WhiteCat}
-						alt="Witch with pointy hat reading a book"
-						ref={(el) => {
-							image2 = el;
-						}}
-					/>
-
-					<div className={CardGen}>
-						<h1 className={HeaderGen} id="project1" >
-							Sorting Hat
-						</h1>
-						<p className="py-10 text-3xl">Quiz web application to sort prospective tech students into relevant tracks of education.</p>
-						<VisibilitySensor onChange={(onChange, setIViz2)}>
-							<button className={ButtonGen} onClick={() => { openOverlay("SortingHat") }}>View My Project</button>
-						</VisibilitySensor>
-					</div>
-				</div>
+				<ProjectCard
+					pic={WhiteCat}
+					picOnLeft={false}
+					picAlt="Vines growing on a white wall"
+					cardTitle="Sorting Hat"
+					cardDesc="Quiz web application to sort prospective tech students into relevant tracks of education."
+					overlayName="SortingHat"
+				/>
 			</section>
 			<section>
-				<div className="flex flex-col md:flex md:flex-row-reverse md:flex-no-wrap">
-					<img
-						className="md:w-1/3 opacity-0 max-full relative object-cover object-center"
-						src={Zebra}
-						alt=""
-						ref={(el) => {
-							image4 = el;
-						}}
-					/>
-
-					<div className={CardGen}>
-						<h1 className={HeaderGen}>African Market Place</h1>
-						<p className=" py-10 text-3xl">A simple small online marketplace for buying and selling wares </p>
-						<VisibilitySensor onChange={(onChange, setIViz4)}>
-							<button className={ButtonGen} onClick={() => { openOverlay("Market") }}>View My Project</button>
-						</VisibilitySensor>
-					</div>
-				</div>
+				<ProjectCard
+					pic={Zebra}
+					picOnLeft={true}
+					picAlt="Vines growing on a white wall"
+					cardTitle="Africa Connected"
+					cardDesc="A simple small online marketplace for posting and viewing wares"
+					overlayName="Market"
+				/>
 			</section>
-			<section >
-				<div className="flex flex-col md:flex md:flex-row md:flex-no-wrap ">
-					<img
-						className="md:w-1/3 opacity-0 max-full relative object-cover object-center"
-						src={Wall}
-						alt=""
-						ref={(el) => {
-							image3 = el;
-						}}
-					/>
-
-					<div className={CardGen}>
-						<h1 className={HeaderGen}>Conway's Game of Life</h1>
-						<p className="py-10 text-3xl">Emergent order from simple rules. Conway's game of life show's the marvel of cellular automatons</p>
-						<VisibilitySensor onChange={(onChange, setIViz3)}>
-							<button className={ButtonGen} onClick={() => { openOverlay("GameOfLife") }}>View My Project</button>
-						</VisibilitySensor>
-					</div>
-				</div>
+			<section>
+				<ProjectCard
+					pic={Wall}
+					picOnLeft={false}
+					picAlt="Vines growing on a white wall"
+					cardTitle="Game of Life"
+					cardDesc="Emergent order from simple rules. Conway's game of life show's the marvel of cellular automatons."
+					overlayName="GameOfLife"
+				/>
 			</section>
 			<ContactNav />
-
 		</div >
 	);
 };
